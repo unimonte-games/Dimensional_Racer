e794aa6033f0eff06a2d57c54c4e9e9fc0921fea
Jordan Savio <jordan.savio.95@gmail.com>
2019-12-12 00:38:08 -0300

    Rework #7 - Menu

---
+++ Assets/Menu/Scripts/Jogar_Interface.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogar_Interface : MonoBehaviour
+{
+
+    public Animation Canvas_Animation;
+    public GameObject[] Characters;
+
+
+
+    public void SelectCharacter()
+    {
+        Canvas_Animation.Play("Interface_IrParaCharacter");
+    }
+
+    public void CharacterSelection(int Char) //Carregar o atual click e desativa todas as anteriores
+    {
+
+        for (int i = 0; i <= 3; i++)
+        {
+            
+            Characters[i].SetActive(false);
+            
+
+        }
+
+        Characters[Char].SetActive(true);
+     
+    }
+
+    public void IrParaLobby()
+    {
+        Canvas_Animation.Play("Interface_IrParaLobby");
+    }
+
+   
+    public void IrParaRoom()
+    {
+        Canvas_Animation.Play("Interface_IrParaRoom");
+    }
+
+    public void VoltarParaLobby()
+    {
+        Canvas_Animation.Play("Interface_VoltarParaLobby");
+    }
+
+    public void VoltarParaCharac()
+    {
+        Canvas_Animation.Play("Interface_VoltarParaCharacter");
+    }
+
+    public void VoltarParaMenu()
+    {
+        Canvas_Animation.Play("Interface_VoltarParaMenu");
+    }
+
+
+}
